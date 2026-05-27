a11096ff1aa5323c3a7966fa1478c67ec67414e3
xuando-0611 <xuan.do@kdi.edu.vn>
2026-05-27 14:03:23 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/quanlikhoKDI/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
